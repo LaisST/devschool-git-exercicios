9b1de155701dbada299304eddc583fbcd92b06b9
Lais.ST <Laiscostast@gmail.com>
2021-10-06 21:00:41 -0300

    inclusão da constante LAIS_TEIXEIRA

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,6 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { LAIS_TEIXEIRA } from "../alunos/lais-teixeira";
 
 export const turma: string[] = [
-    ALAN_JHONNES,
+    ALAN_JHONNES, LAIS_TEIXEIRA
 ]
